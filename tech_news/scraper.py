5c3d59e01c9154debeb55213af5d2ca03a8b2288
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-06-23 16:54:46 -0300

    :sparkles: feat: Implements scrape_noticia function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -3,11 +3,13 @@ from parsel import Selector
 import time
 
 
+HEADER = {"user-agent": "Fake user-agent"}
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui """
+    """Seu código deve vir aqui"""
     try:
-        HEADER = {"user-agent": "Fake user-agent"}
         response = requests.get(url, headers=HEADER, timeout=1)
         time.sleep(1)
         if response.status_code != 200:
@@ -42,8 +44,41 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    news_url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get()
+    news_date = selector.css("li.meta-date::text").get()
+    author = selector.css("span.author > a::text").get()
+    list_summary = selector.css(
+        "div.entry-content p:nth-child(2) *::text"
+    ).getall()
+    summary = ""
+    for text in list_summary:
+        summary += text
+    comments_count = selector.css("comment-content::text").getall()
+    tags = selector.css("section.post-tags ul li > a::text").getall()
+    category = selector.css("a > span.label::text").get()
+    report = {
+        "url": news_url,
+        "title": title,
+        "timestamp": news_date,
+        "writer": author,
+        "comments_count": len(comments_count),
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
+    return report
 
 
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+# REF: https://www.journaldev.com/23625/python-trim-string-rstrip-lstrip-strip
+
+
+if __name__ == "__main__":
+    content = fetch("https://blog.betrybe.com/tecnologia/arquivo-bin/")
+    scrape_noticia(content)
