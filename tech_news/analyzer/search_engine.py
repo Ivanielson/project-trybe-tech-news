0578567beecc8fcd422814aeacf645a024515c04
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-06-24 21:40:05 -0300

    :sparkles: feat: Implements search_by_date function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,22 @@
 from tech_news.database import search_news
 
 
+months = {
+    "01": "janeiro de",
+    "02": "fevereiro de",
+    "03": "março de",
+    "04": "abril de",
+    "05": "maio de",
+    "06": "junho de",
+    "07": "julho de",
+    "08": "agosto de",
+    "09": "setembro de",
+    "10": "outubro de",
+    "11": "novembro de",
+    "12": "dezembro de",
+}
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
@@ -11,9 +27,27 @@ def search_by_title(title):
     return report_list
 
 
+def convert_date_to_long_format(date):
+    split_date = date.split('-')
+    if int(split_date[2]) < 10:
+        new = f"{split_date[2]} de {months[split_date[1]]} {split_date[0]}"
+        return new[1:]
+    else:
+        return f"{split_date[2]} de {months[split_date[1]]} {split_date[0]}"
+
+
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        news_date = convert_date_to_long_format(date)
+        data = search_news({"timestamp": news_date})
+        report_list = []
+        for news in data:
+            report_list.append((news["title"], news["url"]))
+        return report_list
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
