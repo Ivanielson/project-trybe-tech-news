2d1a15b387354ca29f682b540e6b76d33cf5560e
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-06-25 19:39:39 -0300

    :sparkles: feat: Implements top_5_news function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,24 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    data = find_news()
+    report = sorted(
+        data,
+        key=lambda comment: comment["comments_count"],
+        reverse=True
+    )
+    order_list = sorted(
+        report,
+        key=lambda row: row["comments_count"],
+        reverse=True
+    )
+    news_list = []
+    for news in order_list[:5]:
+        news_list.append((news["title"], news["url"]))
+    return news_list
 
 
 # Requisito 11
